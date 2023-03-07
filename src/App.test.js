cf8a3bfa56fbac9a04fc76a72e7b13b4d0ebea69
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2023-03-07 11:05:35 -0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import ProfileSelect from "./ProfileSelect.js";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  const profileURL = "http://localhost:3001/profiles";
+
+  const [profiles, setProfiles] = useState([]);
+  const [activeProfile, setActiveProfile] = useState({
+    id: 0,
+    username: "",
+    collections: [
+      {
+        question: "",
+        correct_answer: "",
+        incorrect_answers: [""],
+      },
+    ],
+  });
+  useEffect(() => {
+    fetch(profileURL)
+      .then((resp) => resp.json())
+      .then((data) => setProfiles(data));
+  }, []);
+
+  const handleSelectProfile = (event) => {
+    const profileToSelect = profiles.find(
+      (profile) => profile.id == event.target.value
+    );
+    console.log(event.target.value);
+    setActiveProfile(profileToSelect);
+  };
+
+  if (activeProfile === activeProfile) {
+  }
+
+  return (
+    <div className="App">
+      <ProfileSelect
+        handleSelectProfile={handleSelectProfile}
+        profiles={profiles}
+        activeProfile={activeProfile}
+      />
+      <hr />
+      <NavBar activeProfile={activeProfile} />
+    </div>
+  );
+}
+
+export default App;
