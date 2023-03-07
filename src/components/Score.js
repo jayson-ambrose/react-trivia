6580152bc650fa8bfceb927a06dc708184bd22bf
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-07 00:25:15 -0700

    score and game systems implemented, still buggy

---
+++ src/components/Score.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Score ({score}) {
+    return (
+        <div>POINTS: {score}</div>
+    )
+}
+
+export default Score;
---
+++ src/components/Strikes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Score ({strikes}) {
+    return (
+        <div style={{color: "#f00"}}>STRIKES: {strikes}</div>
+    )
+}
+
+export default Score;
