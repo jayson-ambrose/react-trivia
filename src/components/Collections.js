64df2ccaa5b39ed7ab30e275c703b5ec910f7f3e
Jayson Ambrose <121517689+jayson-ambrose@users.noreply.github.com>
2023-03-07 12:25:11 -0700

    fixing merge

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-function Collections () {
-    return (
-        <div>Hello World!</div>
-    )
+function CollectionsDisplay({ question }) {
+  const showQuestionInfo = () => {
+    document.getElementById("questionCard").classList.add("d-block");
+    document.getElementById("questionCard").classList.remove("d-none");
+  };
+  return (
+    <>
+      <div id="questionCard" className="col-8 d-none">
+        <h5>{question.question}</h5>
+        <ul className="list-unstyled">
+          <li>{question.correct_answer}</li>
+          {question.incorrect_answers.map((incorrectAnswer) => (
+            <li>{incorrectAnswer}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="col-4">
+        <div>
+          <h5 onClick={showQuestionInfo}>{question.question}</h5>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Collections;
+export default CollectionsDisplay;
