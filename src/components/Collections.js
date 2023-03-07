03df674bd909a889ed887e633af7c40ac6eedec4
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2023-03-07 11:09:45 -0800

    Update and rename SectionThree.js to Collections.js

---
+++ src/components/Collections.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CollectionsDisplay from "./CollectionsDisplay";
+
+function Collections({ activeProfile }) {
+  console.log(activeProfile);
+
+  const collections = activeProfile.collections.map((question) => (
+    <CollectionsDisplay key={question} question={question} />
+  ));
+
+  return (
+    <div className="container">
+      <div className="row">{collections}</div>
+    </div>
+  );
+}
+
+export default Collections;
+/*  */
