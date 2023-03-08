18ee839ee2f569ad0b5a90a456b87e56c434769f
Jayson Ambrose <121517689+jayson-ambrose@users.noreply.github.com>
2023-03-08 09:59:24 -0700

    several changes and integrations
    several changes and integrations

--- src/components/TriviaGame.js
+++ src/components/TriviaGame.js
@@ -3,64 +3,84 @@ import Trivia from "./Trivia.js";
 import Score from "./Score.js";
 import Strikes from "./Strikes.js";
 import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers.js";
+import HighScoreSection from "./HighScoreSection.js"
 
 const fetchUrl = "https://opentdb.com/api.php?amount=1";
 const highScoreUrl = "http://localhost:3001/high-scores";
 
-function TriviaGame({ activeProfile, profileURL }) {
+function TriviaGame({activeProfile}) {
+
   const [questions, setQuestions] = useState([]);
   const [click, setClick] = useState(false);
-  const [score, setScore] = useState(0);
-  const [strikes, setStrikes] = useState(0);
-  const [highScores, setHighScores] = useState([]);
+  const [score, setScore] = useState(0)
+  const [strikes, setStrikes] = useState(0)
+  const [highScores, setHighScores] = useState([])
 
   const handleClick = () => setClick(!click);
 
   useEffect(() => {
     fetch(highScoreUrl)
-      .then((resp) => resp.json())
-      .then((data) => handleHighScores(data));
-  }, []);
+    .then(resp => resp.json())
+    .then(data => setHighScores(data))
+  },[])
 
   const fetchQuery = () => {
+
     fetch(fetchUrl)
       .then((resp) => resp.json())
       .then((data) => setQuestions(data.results));
   };
 
-  const handleGameOver = (pts) => {
-    alert(`GAME OVER, PLEASE TRY AGAIN \n FINAL SCORE: ${pts}`);
+  const handleGameOver  =  (pts) => {
 
-    console.log(activeProfile.username);
-    //process high score stuff with a patch.
+    alert(`GAME OVER, PLEASE TRY AGAIN \n FINAL SCORE: ${pts}`)
 
-    setScore(0);
-    setStrikes(0);
-  };
+    const newScore = {
+      name : activeProfile.username,
+      score : pts 
+    } 
 
-  function processNewHighScore() {}
+    setStrikes(0)
+    setScore(0)
 
-  function handleHighScores(obj) {
-    console.log(obj);
+    console.log(highScores)
+
+
+    fetch(highScoreUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newScore)
+    })
+    .then(resp => resp.json())
+      .then(data => setHighScores([...highScores, data]))
   }
 
-  function startNewGame() {
-    setScore(0);
-    setStrikes(0);
-    fetchQuery();
+  function startNewGame () {
+    setScore(0)
+    setStrikes(0)
+    fetchQuery()
+  }  
+
+  const getHighScores = () => {
+    fetch(highScoreUrl)
+    .then(resp => resp.json())
+    .then(data => setHighScores(data))
   }
 
   const trackScore = (pts) => {
-    setScore(score + pts);
-  };
+    setScore(score + pts)
+  }
 
   const trackStrikes = () => {
-    setStrikes(strikes + 1);
-  };
+    setStrikes(strikes +1)
+  }
 
   if (strikes > 2) {
-    handleGameOver(score);
-  }
+    handleGameOver(score)
+  }  
 
   return (
     <div>
